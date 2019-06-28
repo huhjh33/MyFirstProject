cbb240250614d0769f656f44d78812811b3b4fda
huhjh <huhjh@sk.com>
2019-06-28 16:35:38 +0900

    third test Commit

--- ch01/secondmodule/src/main/java/com/examplehjh/App.java
+++ ch01/secondmodule/src/main/java/com/examplehjh/App.java
@@ -1,5 +1,7 @@
 package com.examplehjh;
 
+import com.examplehjh.util.List;
+
 /**
  * Hello world!
  *
@@ -10,6 +12,24 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("hi han");
-        System.out.printf("%s  %d", "aaaa", 11);
+        System.out.printf("%s  %d\n\n", "aaaa", 11);
+
+        List myList = new List();
+
+        myList.add("aaa1");
+        myList.add("aaa2");
+        myList.add("aaa3");
+        myList.add("aaa4");
+        myList.add("aaa5");
+        myList.add("aaa6");
+        myList.add("aaa7");
+        myList.add("aaa8");
+        myList.add("aaa9");
+        myList.add("aaa10");
+        myList.add("aaa11");
+
+        System.out.println(myList.get(8));
+        System.out.println(myList);
+        System.out.println(myList.toString());
     }
 }
